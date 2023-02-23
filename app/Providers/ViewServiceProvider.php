e4f9b4886a6d2dd7e9ba92f49df19bab901ecbf3
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-24 04:36:36 +0700

    Share default avatar image.

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        View::share('defaultAvatarImage', url('assets/images/avatars/default.jpg'));
+    }
+}
