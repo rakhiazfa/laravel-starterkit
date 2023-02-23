158dd60ba23d8cd372ce40b3121e435e61bf5f1a
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-24 01:50:27 +0700

    Update card styles.

--- resources/views/components/cube/card.blade.php
+++ resources/views/components/cube/card.blade.php
@@ -17,7 +17,7 @@
             @endif
         </div>
     @endif
-    <div class="px-5 pt-5 pb-5">
+    <div class="px-5 {{ $attributes['title'] || count($actions) > 0 ? 'pt-2' : 'pt-5' }} pb-3">
         <div class="w-full overflow-x-auto py-3">
             {{ $slot }}
         </div>
