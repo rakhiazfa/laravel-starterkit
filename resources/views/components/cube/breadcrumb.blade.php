b45e83d0c0dfb33be7453918ba301b1a3e87a011
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-23 23:21:02 +0700

    Update breadcrumb component.

--- resources/views/components/cube/breadcrumb.blade.php
+++ resources/views/components/cube/breadcrumb.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white rounded-lg px-5 py-4 {{ $attributes['class'] }}">
     <nav>
-        <ul class="flex items-center gap-2">
+        <ul class="flex items-center gap-2 text-xs">
             @foreach ($items as $item)
                 <li>
                     <a class="{{ !isset($item['url']) ? 'text-sm select-none pointer-events-none text-gray-400 font-medium' : 'text-sm font-normal text-blue-500 hover:underline' }}"
