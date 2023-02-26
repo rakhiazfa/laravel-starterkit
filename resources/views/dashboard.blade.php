387407e36ffb301dfcc2cbc6fddc6911e5e0f172
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-26 14:15:37 +0700

    Update .wrapper styles.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,5 @@
 <x-cube.auth.layout title="Dashboard">
 
-    <div class="grid gap-7">
 
-        <x-cube.alert type="success" message="Failed to register a new client."></x-cube.alert>
-
-        <x-cube.alert type="error" message="Failed to register a new client."></x-cube.alert>
-
-        <x-cube.alert type="warning" message="Failed to register a new client."></x-cube.alert>
-
-        <x-cube.alert type="info" message="Failed to register a new client."></x-cube.alert>
-
-    </div>
 
 </x-cube.auth.layout>
