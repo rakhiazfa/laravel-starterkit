0c78a79d02302f231e0acf462abad9037ff941e7
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-26 15:58:35 +0700

    Update back button styles.

--- resources/views/components/cube/back_button.blade.php
+++ resources/views/components/cube/back_button.blade.php
@@ -1,4 +1,4 @@
-<a class="flex justify-center items-center w-[40px] h-[40px] bg-blue-400 rounded-md shadow-sm {{ $attributes['class'] }}"
+<a class="flex justify-center items-center w-[40px] h-[40px] bg-blue-500 rounded-md shadow-sm {{ $attributes['class'] }}"
     href="{{ $attributes['url'] ?? url()->previous() }}">
     <i class="uil uil-angle-left text-3xl text-white -ml-1"></i>
 </a>
